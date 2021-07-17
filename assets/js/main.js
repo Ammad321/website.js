334d21b49196d07eab8569df78238ab5e89b2012
Ammad321 <36138097+Ammad321@users.noreply.github.com>
2021-07-17 16:03:34 +0100

    Created some functionalities for vue on item

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,34 @@
 var app = new Vue({
     el: '#app-container',
     data: {
-        
+        items: items,
+        cart: [],
+        isCart: false,
     },
-        
     methods: { 
-        
+        addToCart: function (item) {
+            this.removeSpace(item.id);
+            this.cart.push(item.id);
+        },
+        removeSpace(itemId) {
+            // iterate through each item in items list and check if the id matches than remove 1 space from it
+            for (let i = 0; i < this.items.length; i++) {
+                if(items[i].id === itemId) {
+                    items[i].space -= 1;
+                }
+            }
+        },
+        IsPossibleToAddToCart(item) {
+            // Check if there is an item left in the item catalog if not it returns false else it returns true
+            if (item.space != 0 ) {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        MoveToCart() {
+            this.isShoppingCart = this.isShoppingCart ? false : true;
+        },
     },
     computed: {
         
